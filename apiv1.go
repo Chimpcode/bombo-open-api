2c0e321db3f03c1fbf25588c7538eafca150b1ee
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2018-03-03 02:39:51 -0500

    trying to work

--- apiv1.go
+++ apiv1.go
@@ -4,18 +4,23 @@ import (
 	"github.com/kataras/iris"
 	"io/ioutil"
 	"encoding/json"
+	"github.com/k0kubun/pp"
+	"log"
 )
 
 func LinkApi(api iris.Party, manager *WorkManager) {
 
 	api.Get("/{endpoint: string}", func(c iris.Context) {
 		endpoint := c.Params().Get("endpoint")
+		pp.Println(endpoint)
 		for _, work := range manager.Works {
 			if work.Name == endpoint {
 				filePath := "./data/" + work.Type + "_" +work.Name + ".json"
 
+				pp.Println(filePath)
 				data, err := ioutil.ReadFile(filePath)
 				if err != nil{
+					log.Println("Error at ioutil.ReadFile()")
 					c.StatusCode(iris.StatusInternalServerError)
 					c.JSON(iris.Map{
 						"data": nil,
